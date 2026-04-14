ac6b5e19ac0a33bd69540f0ab5b94171f4cf1172
Mohamad Hanifan <mohamad.hanifan@quipper.com>
2026-04-14 16:17:48 +0700

    feat: debug vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,8 @@
-'use client'
-
-import { useEffect, useState } from 'react'
 import RequestDashboard from '@/components/RequestDashboard'
 
-function generateClientId(): string {
-  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
-  let result = ''
-  for (let i = 0; i < 8; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length))
-  }
-  return result
-}
-
 export default function Home() {
-  const [clientId, setClientId] = useState<string>('')
-
-  useEffect(() => {
-    let id = localStorage.getItem('client_id')
-    if (!id) {
-      id = generateClientId()
-      localStorage.setItem('client_id', id)
-    }
-    setClientId(id)
-  }, [])
-
-  if (!clientId) return null
+  // Generate a client ID on the server for SSR
+  const clientId = 'default' // This will be overridden by the client component
 
   return <RequestDashboard clientId={clientId} />
 }
